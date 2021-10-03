dabec1dbb97960530bc31d76c3b7dc957bf38db1
Sandhya-igdtuw <manjucs8800@gmail.com>
2021-10-04 00:01:32 +0530

    carrental first commit

---
+++ admin/includes/config.php
@@ -0,0 +1,22 @@
+<?php 
+// DB credentials.
+// define('DB_HOST','localhost');
+// define('DB_USER','u655417478_root');
+// define('DB_PASS','123456');
+// define('DB_NAME','u655417478_carrental');
+// define('DB_PORT','3307');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','carrental');
+define('DB_PORT','3307');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST."; port= 3307; dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
